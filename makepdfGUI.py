f76a82c1c57bb37c717a2cd6274897c1dcf91e44
dinosauria123 <dinosaur@db3.so-net.ne.jp>
2020-01-18 00:28:55 +0900

    separate GUI from main thread

--- makepdfGUI.py
+++ makepdfGUI.py
@@ -25,7 +25,7 @@ class Application(tk.Frame):
     def create_widgets(self):
         global var
         var = tk.StringVar()
-        var.set("Select image directory")
+        var.set("Set output pdf file")
         self.lbl = tk.Label(root, textvariable=var)
         self.lbl.place(x=50, y=10)
 
@@ -36,17 +36,17 @@ class Application(tk.Frame):
 
         self.button2 = tk.Button(root)
         self.button2["text"] = "Set IMG Dir"
-        self.button2["command"] = self.thread
+        self.button2["command"] = self.setimgdir
         self.button2.place(x=120, y=50)
 
         self.button3 = tk.Button(root)
         self.button3["text"] = "Config"
         self.button3["command"] = self.create_window
-        self.button3.place(x=210, y=50)
+        self.button3.place(x=205, y=50)
 
         self.quit = tk.Button(root, text="Quit")
         self.quit["command"] = self.appQuit
-        self.quit.place(x=300, y=50)
+        self.quit.place(x=330, y=50)
 
     def create_window(self):
         global t, text
@@ -62,12 +62,12 @@ class Application(tk.Frame):
         button3["text"] = "Save"
         button3["command"] = self.saveconfig
 
-    def makepdf(self):
+    def setimgdir(self):
+        global APIKEY, files, imgdir
 
         if pdffile=="":
             messagebox.showerror("Error", "Set pdf file before set images")
             self.savepdf()
-            sys.exit(1)
 
         imgdir = filedialog.askdirectory(initialdir = dir)
         
@@ -75,8 +75,6 @@ class Application(tk.Frame):
             files = sorted(glob.glob(imgdir+"/*jpg"))
         except TypeError:                                  # cancel
             sys.exit(1)
-
-        gcvocr = ntpath.dirname(__file__)+"/gcvocr.exe"
 
         confpath = ntpath.dirname(__file__)+"/config.txt"
         try:
@@ -92,15 +90,47 @@ class Application(tk.Frame):
             self.create_window()
             sys.exit(1)
         
-        if savedir == "":
-            messagebox.showerror("Error", "Set output pdf file")
-            self.savepdf()
-            sys.exit(1)
+        Thread().start()
+ 
+    def appQuit(self):
+        Application.destroy(self)
+        sys.exit(0)
 
+    def saveconfig(self):
+        ctext = text.get()
+        if ctext == "":
+            messagebox.showerror("Error", "No API KEY") 
+            t.destroy()
+            sys.exit(1)
+        path = ntpath.dirname(__file__)+"/config.txt"
+        with open(path, mode="w") as f:
+            f.write(ctext)
+        t.destroy()
+
+    def savepdf(self):
+        global pdffile, savedir
+        pdffile = filedialog.asksaveasfilename(filetypes = [("pdf file","*.pdf")], initialdir = dir)
+        if pdffile == "":
+            exit(1)     
+        savedir = ntpath.dirname(pdffile)
+        try:
+            os.mkdir(savedir + "/temp")
+        except FileExistsError:
+            pass
+        var.set("Select image directry")
+
+
+class Thread(threading.Thread):
+   def __init__(self):
+        threading.Thread.__init__(self)
+        self.daemon = True
+
+   def run(self):
+        gcvocr = ntpath.dirname(__file__)+"/gcvocr.exe"
         for name in files:
             print("google OCR", ntpath.basename(name))
             var.set("google OCR " + ntpath.basename(name))
-            command = [gcvocr, name, APIKEY, savedir + "/temp"]
+            command = [gcvocr, name, APIKEY, savedir + "/temp/"]
             subprocess.call(command)
  
             print ("Convert ", ntpath.basename(name) ,"to hocr")
@@ -123,31 +153,6 @@ class Application(tk.Frame):
         print("Done!")
         var.set("Done!")
         sys.exit(1)
-
-    def thread(self):
-        global th
-        th = threading.Thread(target=self.makepdf)
-        th.start()
-
-    def appQuit(self):
-        sys.exit(0)
-
-    def saveconfig(self):
-        ctext = text.get()
-        if ctext == "":
-            messagebox.showerror("Error", "No API KEY") 
-            t.destroy()
-            sys.exit(1)
-        path = ntpath.dirname(__file__)+"/config.txt"
-        with open(path, mode="w") as f:
-            f.write(ctext)
-        t.destroy()
-
-    def savepdf(self):
-        global pdffile, savedir
-        pdffile = filedialog.asksaveasfilename(filetypes = [("pdf file","*.pdf")], initialdir = dir)        
-        savedir = ntpath.dirname(pdffile)
-        os.mkdir(savedir + "/temp")
 
 def move_glob(dst_path, pathname, recursive=True):
     for p in glob.glob(pathname, recursive=recursive):
